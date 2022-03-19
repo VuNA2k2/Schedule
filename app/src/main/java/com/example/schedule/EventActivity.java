6d25fa7c04bbc53cfc57eb62bab3df2dc6c609d8
VuNAP <vuvovan2k2>
2022-03-19 20:27:04 +0700

    Intro

--- app/src/main/java/com/example/schedule/EventActivity.java
+++ app/src/main/java/com/example/schedule/EventActivity.java
@@ -130,4 +130,9 @@ public class EventActivity extends AppCompatActivity{
         super.onBackPressed();
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/schedule/IntroActivity.java
@@ -0,0 +1,40 @@
+package com.example.schedule;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    private TextView txtAppName;
+    private ImageView imgLogo;
+    private Animation up, down;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity);
+        init();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },1500);
+    }
+
+    private void init() {
+        txtAppName = (TextView) findViewById(R.id.txtAppName);
+        imgLogo = (ImageView) findViewById(R.id.imgLogo);
+        up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_up);
+        down = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_down);
+        txtAppName.setAnimation(down);
+        imgLogo.setAnimation(up);
+    }
+}
